cc65b48ee9158fc6631355add9832f15be2658f6
Alan Andrés Valderrama Lame <alan.valderrama@correounivalle.edu.co>
2025-05-09 08:29:37 -0500

    Se vuelve a dejar el Diseases cómo estaba originalmente.

--- src/pages/diseases/Diseases.jsx
+++ src/pages/diseases/Diseases.jsx
@@ -4,43 +4,46 @@ import { Link } from "react-router-dom";
 import ContentModule from "../../components/ContentModule/ContentModule";
 
 const Disease = () => {
-  return (
-    <div className="disease-container">
-      <div id = "diseases" className = "diseases">
-        <ContentModule
-          type="primary"
-          backgroundImage="epoc.png"
-          size={"100vh"}
-        >
-          <a href="/epoc"><h1>EPOC</h1></a>
-        </ContentModule>
+    return (
+      <>
+        <div className="disease-container">
+          <div id = "diseases" className = "diseases">
+            <ContentModule
+              type="primary"
+              backgroundImage="epoc.png"
+              size={"100vh"}
+            >
+              <a href="/epoc"><h1>EPOC</h1></a>
+            </ContentModule>
 
-        <ContentModule
-          type="primary"
-          backgroundImage="asthma.png"
-          size={"100vh"}
-        >
-          <Link to="/asthma"><h1>ASMA</h1></Link>
-        </ContentModule>
 
-        <ContentModule
-          type="primary"
-          backgroundImage="pneumonia.png" 
-          size={"100vh"}
-        > 
-          <a href="/pneumonia"><h1>NEUMONÍA</h1></a>
-        </ContentModule>
+            <ContentModule
+              type="primary"
+              backgroundImage="asthma.png"
+              size={"100vh"}
+            >
+           <Link to="/asthma"><h1>ASMA</h1></Link>
+            </ContentModule>
 
-        <ContentModule    
-        type="primary"
-        backgroundImage="embolism.png"
-        size={"100vh"}
-        >
-          <a href="/embolism"><h1>EMBOLIA PULMONAR</h1></a>
-        </ContentModule>
-      </div>
-    </div> 
-  );
-};
+            <ContentModule
+              type="primary"
+              backgroundImage="pneumonia.png" 
+              size={"100vh"}  
+            > 
+            <a href="/pneumonia"><h1>NEUMONÍA</h1></a>
+            </ContentModule>
 
-export default Disease;
+            <ContentModule    
+            type="primary"
+            backgroundImage="embolism.png"
+            size={"100vh"}
+            >
+            <a href="/embolism"><h1>EMBOLIA PULMONAR</h1></a>
+            </ContentModule>
+          </div>
+        </div> 
+      </>
+    );
+  };
+  
+  export default Disease;
